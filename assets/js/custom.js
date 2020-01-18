d3d9e98ece8080a5fd59bdfe16ba2424dcb54bbe
lcmou <lichao.dlr@gmail.com>
2020-01-18 21:00:59 +0100

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,92 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scroll_fun: function () {
+
+            /*====================================
+             SCROLL SCRIPTS 
+            ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+            
+
+        },
+        
+        gallery_fun: function () {
+            /*====================================
+                 FOR IMAGE/GALLERY POPUP
+            ======================================*/
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            /*====================================
+            FOR IMAGE/GALLERY FILTER
+            ======================================*/
+
+            // MixItUp plugin
+            // http://mixitup.io
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+        vedio_fun:function()
+        {
+            $(function () {
+                $(".player").mb_YTPlayer();
+            });
+        },
+
+        nice_scroll_fun:function()
+        {
+            $("html").niceScroll();
+        },
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.scroll_fun();
+        mainApp.gallery_fun();
+        mainApp.vedio_fun();
+        mainApp.nice_scroll_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
